056a9f7af7526a2a6cd37603ab70b9d14bf46c09
Armando Ruiz <aranruiz13@gmail.com>
2021-10-26 21:43:54 -0500

    Add solution for two sum

---
+++ src/main/kotlin/linkedlist/easy/TwoSum.kt
@@ -0,0 +1,17 @@
+package linkedlist.easy
+
+// https://leetcode.com/problems/two-sum/
+class TwoSumSolution {
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val auxiliaryMap = HashMap<Int, Int>()
+        nums.forEachIndexed { index, value ->
+            if (auxiliaryMap.containsKey(target - value)) {
+                val firstIndex = auxiliaryMap[target - value]!!
+                return intArrayOf(firstIndex, index)
+            }
+            auxiliaryMap[value] = index
+        }
+        return intArrayOf()
+    }
+}
