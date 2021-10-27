93db2bf9c556db84d84b5ed3601ed88361947a71
Armando Ruiz <aranruiz13@gmail.com>
2021-10-26 22:18:38 -0500

    Add solution for palindrome number

---
+++ src/main/kotlin/linkedlist/easy/PalindromeNumberSolution.kt
@@ -0,0 +1,16 @@
+package linkedlist.easy
+
+class PalindromeNumberSolution {
+
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+        var aux = x
+        var result = 0
+        while (aux != 0) {
+            result *= 10
+            result += aux % 10
+            aux /= 10
+        }
+        return result == x
+    }
+}
