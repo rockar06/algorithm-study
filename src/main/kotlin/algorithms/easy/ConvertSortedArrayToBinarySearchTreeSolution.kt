ece9e2a8887568fa0f963475ace104fba1b631a2
Armando Ruiz <aranruiz13@gmail.com>
2021-11-30 22:05:23 -0600

    Improve solution for convert sorted array to binary search tree

--- src/main/kotlin/algorithms/easy/ConvertSortedArrayToBinarySearchTreeSolution.kt
+++ src/main/kotlin/algorithms/easy/ConvertSortedArrayToBinarySearchTreeSolution.kt
@@ -4,14 +4,10 @@ import algorithms.common.TreeNode
 
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 class ConvertSortedArrayToBinarySearchTreeSolution {
-    fun sortedArrayToBST(nums: IntArray): TreeNode? = when(nums.size) {
-        1 -> TreeNode(nums[0])
-        2 -> TreeNode(nums[1]).apply {
-            left = TreeNode(nums[0])
-        }
-        else -> TreeNode(nums[nums.size / 2]).apply {
-            left = sortedArrayToBST(nums.copyOfRange(0, nums.size / 2))
-            right = sortedArrayToBST(nums.copyOfRange((nums.size / 2) + 1, nums.size))
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        return TreeNode(nums[nums.size / 2]).apply {
+            left = if (nums.size > 1) sortedArrayToBST(nums.copyOfRange(0, nums.size / 2)) else null
+            right = if (nums.size > 2) sortedArrayToBST(nums.copyOfRange((nums.size / 2) + 1, nums.size)) else null
         }
     }
 }
