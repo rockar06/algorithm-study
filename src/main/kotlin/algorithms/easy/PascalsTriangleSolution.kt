2661a04741ec3d9da4222c990524cfb77e84ecfc
Armando Ruiz <aranruiz13@gmail.com>
2021-12-09 21:48:43 -0600

    Add another solution for Pascal's Triangle

--- src/main/kotlin/algorithms/easy/PascalsTriangleSolution.kt
+++ src/main/kotlin/algorithms/easy/PascalsTriangleSolution.kt
@@ -2,7 +2,38 @@ package algorithms.easy
 
 // https://leetcode.com/problems/pascals-triangle/submissions/
 class PascalsTriangleSolution {
+
     fun generate(numRows: Int): List<List<Int>> {
+        val result = arrayListOf<List<Int>>()
+        var tempArray = arrayListOf<Int>()
+        var levelIndex = 0
+        var rowIndex = 0
+        var counter = 0
+
+        while (levelIndex < numRows) {
+            counter++
+            if (rowIndex == 0 || rowIndex == levelIndex) {
+                tempArray.add(1)
+            } else {
+                val temp = result[levelIndex - 1]
+                val tempItem = temp[rowIndex] + temp[rowIndex - 1]
+                tempArray.add(tempItem)
+            }
+
+            if (rowIndex < levelIndex) {
+                rowIndex++
+            } else {
+                levelIndex++
+                rowIndex = 0
+                result.add(tempArray)
+                tempArray = arrayListOf()
+            }
+        }
+        return result
+    }
+
+
+/*    fun generate(numRows: Int): List<List<Int>> {
         val result = arrayListOf<List<Int>>()
         for (line in 0 until numRows) {
             val innerResult = arrayListOf<Int>()
@@ -21,5 +52,5 @@ class PascalsTriangleSolution {
             result.add(innerResult)
         }
         return result
-    }
+    }*/
 }
