348b3b23c20b2098e6a8437334042cd1fefdd616
Armando Ruiz <aranruiz13@gmail.com>
2021-12-30 18:08:53 -0600

    Add solution for Word Pattern

---
+++ src/main/kotlin/algorithms/easy/WordPatternSolution.kt
@@ -0,0 +1,19 @@
+package algorithms.easy
+
+class WordPatternSolution {
+    fun wordPattern(pattern: String, s: String): Boolean {
+        val segmentedString = s.split(" ")
+        if (pattern.length != segmentedString.size) return false
+        val charMap = hashMapOf<Char, String>()
+        pattern.forEachIndexed { index, c ->
+            if ((charMap[c] != null && charMap[c] != segmentedString[index]) ||
+                (charMap.containsValue(segmentedString[index]) && c != charMap.keys.firstOrNull { it == c })
+            ) {
+                return false
+            } else {
+                charMap[c] = segmentedString[index]
+            }
+        }
+        return true
+    }
+}
