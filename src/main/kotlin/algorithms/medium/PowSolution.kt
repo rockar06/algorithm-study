fab20c1a94e67ed1a254234c53d6af521442f744
Armando Ruiz <aranruiz13@gmail.com>
2022-01-04 08:29:48 -0600

    Add another solution for Pow

--- src/main/kotlin/algorithms/medium/PowSolution.kt
+++ src/main/kotlin/algorithms/medium/PowSolution.kt
@@ -1,8 +1,10 @@
 package algorithms.medium
 
+import kotlin.math.abs
+
 // https://leetcode.com/problems/powx-n/submissions/
 class PowSolution {
-
+/*
     private val positiveOne = 1.toDouble()
     private val negativeOne = (-1).toDouble()
 
@@ -34,6 +36,35 @@ class PowSolution {
                 if (result < -100000 || result > 100000) return tempResult
                 powCounter++
             }
+        }
+        return result
+    }*/
+
+
+    private val positiveOne = 1.toDouble()
+    private val negativeOne = (-1).toDouble()
+
+    fun myPow(x: Double, n: Int): Double {
+        if (n == 0 || x == positiveOne) return positiveOne
+        if (x == negativeOne && (n != 1 && n % 2 == 0)) {
+            return positiveOne
+        } else if (x == negativeOne && n % 2 != 0) {
+            return negativeOne
+        }
+        var exponent = n
+        var base = x
+        var result: Double = 1.toDouble()
+        if (n < 0) {
+            base = 1 / x
+            exponent = if (exponent == Int.MIN_VALUE) {
+                Int.MAX_VALUE
+            } else {
+                abs(exponent)
+            }
+        }
+        for (counter in 0 until exponent) {
+            result *= base
+            if (counter > 1 && result == 0.toDouble()) return 0.toDouble()
         }
         return result
     }
