2717801a5990ead67a785bfb4e37427108f79c13
Armando Ruiz <aranruiz13@gmail.com>
2022-01-14 17:21:44 -0600

    Add solution for Permutation In String

--- src/main/kotlin/algorithms/medium/PermutationInStringSolution.kt
+++ src/main/kotlin/algorithms/medium/PermutationInStringSolution.kt
@@ -3,40 +3,27 @@ package algorithms.medium
 // https://leetcode.com/problems/permutation-in-string/
 class PermutationInStringSolution {
 
-    private val permutationsMap = hashMapOf<String, Int>()
-
     fun checkInclusion(s1: String, s2: String): Boolean {
-        if (s1.length > s2.length) return false
-        permute(s1, 0, s1.length - 1)
-        var firstIndex = 0
-        var lastIndex = s1.length
-        while (lastIndex <= s2.length) {
-            if (permutationsMap[s2.substring(firstIndex, lastIndex)] == 1) return true
-            firstIndex++
-            lastIndex++
+        val charArray = IntArray(26)
+        s1.forEach {
+            charArray[it - 'a']++
         }
+
+        for (firstIndex in 0..(s2.length - s1.length)) {
+            val wordCharArray = IntArray(26)
+            for (secondIndex in s1.indices) {
+                wordCharArray[s2[firstIndex + secondIndex] - 'a']++
+            }
+            if (matchEveryChar(charArray, wordCharArray)) return true
+        }
+
         return false
     }
 
-    private fun permute(resource: String, startIndex: Int, endIndex: Int) {
-        var str: String = resource
-        if (startIndex == endIndex) {
-            permutationsMap[str] = 1
-        } else {
-            for (i in startIndex..endIndex) {
-                str = swap(str, startIndex, i)
-                permute(str, startIndex + 1, endIndex)
-                str = swap(str, startIndex, i)
-            }
+    private fun matchEveryChar(charArray: IntArray, wordCharArray: IntArray): Boolean {
+        for (index in 0 until 26) {
+            if (charArray[index] != wordCharArray[index]) return false
         }
-    }
-
-    private fun swap(resource: String, startIndex: Int, endIndex: Int): String {
-        val temp: Char
-        val charArray = resource.toCharArray()
-        temp = charArray[startIndex]
-        charArray[startIndex] = charArray[endIndex]
-        charArray[endIndex] = temp
-        return String(charArray)
+        return true
     }
 }
