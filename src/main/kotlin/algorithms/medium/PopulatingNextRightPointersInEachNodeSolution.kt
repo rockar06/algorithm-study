ae669aed9915ee52048696f15d0cd3f3b4ff3c22
Armando Ruiz <aranruiz13@gmail.com>
2022-01-16 13:42:59 -0600

    Add solution for Populating Next Right Pointers in Each Node

---
+++ src/main/kotlin/algorithms/medium/PopulatingNextRightPointersInEachNodeSolution.kt
@@ -0,0 +1,29 @@
+package algorithms.medium
+
+import algorithms.common.Node
+
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/
+class PopulatingNextRightPointersInEachNodeSolution {
+
+    fun connect(root: Node?): Node? {
+        if (root == null) return null
+
+        var leftMost = root
+
+        while (leftMost?.left != null) {
+            var head = leftMost
+
+            while (head != null) {
+                head.left?.next = head.right
+                /*head.next?.let {
+                    head?.right?.next = it.next?.left
+                }*/
+                head.right?.next = head.next?.left
+                head = head.next
+            }
+            leftMost = leftMost.left
+        }
+
+        return root
+    }
+}
