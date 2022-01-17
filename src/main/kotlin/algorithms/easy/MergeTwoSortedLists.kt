7ab8be5b270960353dc154a4f75c0c8575e3d94e
Armando Ruiz <aranruiz13@gmail.com>
2022-01-17 10:21:21 -0600

    Improve solution for Merge Two Sorted Lists

--- src/main/kotlin/algorithms/easy/MergeTwoSortedLists.kt
+++ src/main/kotlin/algorithms/easy/MergeTwoSortedLists.kt
@@ -6,23 +6,16 @@ import algorithms.common.ListNode
 class MergeTwoSortedLists {
 
     fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-        if (l1 == null && l2 == null) return null
         return if (l1 != null && l2 == null) {
-            l1.apply {
-                next = mergeTwoLists(l1.next, null)
-            }
+            l1
         } else if (l1 == null && l2 != null) {
-            l2.apply {
-                next = mergeTwoLists(null, l2.next)
-            }
+            l2
         } else if ((l1?.value ?: -1) < (l2?.value ?: -1)) {
-            l1?.apply {
-                next = mergeTwoLists(l1.next, l2)
-            }
+            l1?.next = mergeTwoLists(l1?.next, l2)
+            l1
         } else {
-            l2?.apply {
-                next = mergeTwoLists(l1, l2.next)
-            }
+            l2?.next = mergeTwoLists(l1, l2?.next)
+            l2
         }
     }
 }
