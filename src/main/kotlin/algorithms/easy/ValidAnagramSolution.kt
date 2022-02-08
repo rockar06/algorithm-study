04a2445bb3ec7192dd3e5b7c9988494280c3ec80
Armando Ruiz <aranruiz13@gmail.com>
2022-02-08 16:13:29 -0600

    Add another solution for Valid Anagram

--- src/main/kotlin/algorithms/easy/ValidAnagramSolution.kt
+++ src/main/kotlin/algorithms/easy/ValidAnagramSolution.kt
@@ -2,7 +2,7 @@ package algorithms.easy
 
 // https://leetcode.com/problems/valid-anagram/submissions/
 class ValidAnagramSolution {
-    fun isAnagram(s: String, t: String): Boolean {
+    /*fun isAnagram(s: String, t: String): Boolean {
         if (s.length != t.length) return false
         val charMap = hashMapOf<Char, Int>()
         s.forEach {
@@ -13,6 +13,21 @@ class ValidAnagramSolution {
         }
         for (value in charMap.values) {
             if (value != 0) return false
+        }
+        return true
+    }*/
+
+    fun isAnagram(s: String, t: String): Boolean {
+        val firstCharMap = IntArray(26)
+        val secondCharMap = IntArray(26)
+        s.forEach {
+            firstCharMap[it - 'a']++
+        }
+        t.forEach {
+            secondCharMap[it - 'a']++
+        }
+        for (index in 0 until 26) {
+            if (firstCharMap[index] != secondCharMap[index]) return false
         }
         return true
     }
