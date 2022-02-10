252f0583f0504ab159ca2fbf54bf5e8d800d6be2
Armando Ruiz <aranruiz13@gmail.com>
2022-02-10 09:16:11 -0600

    Add recursive solution for Merge Two Sorted Lists

--- src/main/kotlin/algorithms/easy/MergeTwoSortedLists.kt
+++ src/main/kotlin/algorithms/easy/MergeTwoSortedLists.kt
@@ -5,7 +5,7 @@ import algorithms.common.ListNode
 // https://leetcode.com/problems/merge-two-sorted-lists/
 class MergeTwoSortedLists {
 
-    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+    /*fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
         return if (l1 != null && l2 == null) {
             l1
         } else if (l1 == null && l2 != null) {
@@ -17,5 +17,40 @@ class MergeTwoSortedLists {
             l2?.next = mergeTwoLists(l1, l2?.next)
             l2
         }
+    }*/
+
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (l1 != null && l2 == null) {
+            return l1
+        } else if (l1 == null && l2 != null) {
+            return l2
+        }
+        var result: ListNode? = null
+        var resultHead = result
+        var headL1 = l1
+        var headL2 = l2
+        while (headL1 != null && headL2 != null) {
+            if (headL1.value < headL2.value) {
+                if (result == null) {
+                    result = ListNode(headL1.value)
+                    resultHead = result
+                } else {
+                    resultHead?.next = ListNode(headL1.value)
+                    resultHead = resultHead?.next
+                }
+                headL1 = headL1.next
+            } else {
+                if (result == null) {
+                    result = ListNode(headL2.value)
+                    resultHead = result
+                } else {
+                    resultHead?.next = ListNode(headL2.value)
+                    resultHead = resultHead?.next
+                }
+                headL2 = headL2.next
+            }
+        }
+        resultHead?.next = headL1 ?: headL2
+        return result
     }
 }
