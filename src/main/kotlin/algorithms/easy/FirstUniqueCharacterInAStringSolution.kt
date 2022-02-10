1e8230e781a64b8ec53642c90161eb1fc8bce6ab
Armando Ruiz <aranruiz13@gmail.com>
2022-02-09 19:06:36 -0600

    Add another solution for First Unique Character In A String

--- src/main/kotlin/algorithms/easy/FirstUniqueCharacterInAStringSolution.kt
+++ src/main/kotlin/algorithms/easy/FirstUniqueCharacterInAStringSolution.kt
@@ -3,13 +3,24 @@ package algorithms.easy
 // https://leetcode.com/problems/first-unique-character-in-a-string/
 class FirstUniqueCharacterInAStringSolution {
 
-    fun firstUniqChar(s: String): Int {
+    /*fun firstUniqChar(s: String): Int {
         val charMapper = hashMapOf<Char, Int>()
         s.forEach {
             charMapper[it] = charMapper.getOrDefault(it, 0) + 1
         }
         s.forEachIndexed { index, item ->
             if (charMapper[item] == 1) return index
+        }
+        return -1
+    }*/
+
+    fun firstUniqChar(s: String): Int {
+        val charMapper = IntArray(26)
+        s.forEach {
+            charMapper[it - 'a']++
+        }
+        s.forEachIndexed { index, item ->
+            if (charMapper[item - 'a'] == 1) return index
         }
         return -1
     }
