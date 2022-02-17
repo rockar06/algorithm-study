7e226dea22a4d55bd916e90a7fc1ba8f1fc6b9f9
Armando Ruiz <aranruiz13@gmail.com>
2022-02-17 17:51:45 -0600

    Add another solution for Binary Two Sum IV

--- src/main/kotlin/algorithms/easy/TwoSumIVSolution.kt
+++ src/main/kotlin/algorithms/easy/TwoSumIVSolution.kt
@@ -5,14 +5,12 @@ import algorithms.common.TreeNode
 // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
 class TwoSumIVSolution {
 
-    private val mappedItems = hashMapOf<Int, Int>()
-
-    fun findTarget(root: TreeNode?, k: Int): Boolean {
+    fun findTarget(root: TreeNode?, k: Int, mappedItems: HashMap<Int, Int> = hashMapOf()): Boolean {
         root?.let {
             val complement = k - it.`val`
             return if (mappedItems[complement] == null) {
                 mappedItems[it.`val`] = complement
-                findTarget(it.left, k) || findTarget(it.right, k)
+                findTarget(it.left, k, mappedItems) || findTarget(it.right, k, mappedItems)
             } else {
                 true
             }
