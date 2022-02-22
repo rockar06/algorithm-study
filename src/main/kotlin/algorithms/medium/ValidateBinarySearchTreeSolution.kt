654a0bae2df488b23dd677db651ccbe6d5fad4bd
Armando Ruiz <aranruiz13@gmail.com>
2022-02-21 21:33:43 -0600

    Add possible solution for Invert Binary Tree

--- src/main/kotlin/algorithms/medium/ValidateBinarySearchTreeSolution.kt
+++ src/main/kotlin/algorithms/medium/ValidateBinarySearchTreeSolution.kt
@@ -1,10 +1,11 @@
 package algorithms.medium
 
 import algorithms.common.TreeNode
+import kotlin.math.max
 
 class ValidateBinarySearchTreeSolution {
 
-    fun isValidBST(root: TreeNode?, parentNode: TreeNode? = null): Boolean {
+    fun isValidBST(root: TreeNode?, minimumValue: Int? = null, maximumValue: Int? = null): Boolean {
         root?.let {
             if (it.left == null && it.right == null) {
                 return true
@@ -13,8 +14,8 @@ class ValidateBinarySearchTreeSolution {
                 if (leftItem.`val` >= it.`val`) {
                     return false
                 }
-                parentNode?.let { parentItem ->
-                    if (leftItem.`val` < parentItem.`val`) {
+                minimumValue?.let {
+                    if (leftItem.`val` < minimumValue) {
                         return false
                     }
                 }
@@ -23,28 +24,62 @@ class ValidateBinarySearchTreeSolution {
                 if (rightItem.`val` <= it.`val`) {
                     return false
                 }
-                parentNode?.let { parentItem ->
-                    if (rightItem.`val` > parentItem.`val`) {
+                maximumValue?.let {
+                    if (rightItem.`val` > maximumValue) {
                         return false
                     }
                 }
             }
-            return isValidBST(it.left, it) && isValidBST(it.right, it)
+            return isValidBST(it.left, maximumValue = maximumValue ?: it.`val`) &&
+                    isValidBST(it.right, minimumValue = minimumValue ?: it.`val`)
         }
         return true
     }
+
+    /*
+        fun isValidBST(root: TreeNode?, rootTreeValue: Int? = null): Boolean {
+        root?.let {
+            if (it.left == null && it.right == null) {
+                return true
+            }
+            it.left?.let { leftItem ->
+                if (leftItem.`val` >= it.`val`) {
+                    return false
+                }
+                rootTreeValue?.let {
+                    if (leftItem.`val` < rootTreeValue) {
+                        return false
+                    }
+                }
+            }
+            it.right?.let { rightItem ->
+                if (rightItem.`val` <= it.`val`) {
+                    return false
+                }
+                rootTreeValue?.let {
+                    if (rightItem.`val` > rootTreeValue) {
+                        return false
+                    }
+                }
+            }
+            val rootValue = rootTreeValue ?: it.`val`
+            return isValidBST(it.left, rootTreeValue = rootValue) && isValidBST(it.right, rootTreeValue = rootValue)
+        }
+        return true
+    }
+     */
 }
 
 // [3,1,5,0,2,4,6]
 fun main() {
-    val tree = TreeNode(5).apply {
+    /*val tree = TreeNode(5).apply {
         left = TreeNode(4)
         right = TreeNode(6).apply {
             left = TreeNode(3)
             right = TreeNode(7)
         }
-    }
-    /*val tree = TreeNode(3).apply {
+    }*/
+    val tree = TreeNode(3).apply {
         left = TreeNode(1).apply {
             left = TreeNode(0)
             right = TreeNode(2)
@@ -53,7 +88,7 @@ fun main() {
             left = TreeNode(4)
             right = TreeNode(6)
         }
-    }*/
+    }
     val result = ValidateBinarySearchTreeSolution().isValidBST(tree)
     result
 }
