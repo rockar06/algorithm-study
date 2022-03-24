76fc5d6f40a219b679250b702737922754af2416
Armando Ruiz <aranruiz13@gmail.com>
2022-03-24 08:29:23 -0600

    Add solution for Find Peak Element

---
+++ src/main/kotlin/algorithms/medium/FindPeakElementSolution.kt
@@ -0,0 +1,38 @@
+package algorithms.medium
+
+// https://leetcode.com/problems/find-peak-element
+class FindPeakElementSolution {
+
+    fun findPeakElement(nums: IntArray): Int {
+        if (nums.size == 1) return 0
+        for (index in nums.indices step 2) {
+            if (validateNumber(nums, index)) {
+                return index
+            }
+        }
+
+        for (index in 1 until nums.size step 2) {
+            if (validateNumber(nums, index)) {
+                return index
+            }
+        }
+        return 0
+    }
+
+    private fun validateNumber(nums: IntArray, index: Int): Boolean {
+        return if ((index - 1) >= 0 && (index + 1) <= nums.size - 1) {
+            nums[index] > nums[index - 1] && nums[index] > nums[index + 1]
+        } else if ((index - 1) >= 0 && (index + 1) >= nums.size) {
+            nums[index] > nums[index - 1]
+        } else {
+            nums[index] > nums[index + 1]
+        }
+    }
+}
+
+fun main() {
+    //val testCase = intArrayOf(1,2,1,3,5,6,4)
+    val testCase = intArrayOf(1)
+    val result = FindPeakElementSolution().findPeakElement(testCase)
+    result
+}
