7e89ebaccbfa7de87c82f383f1d516d400973e26
Armando Ruiz <aranruiz13@gmail.com>
2022-04-14 23:11:26 -0500

    Add solutions for Maximum Average Subarray I

--- src/main/kotlin/algorithms/easy/RangeSumOfBSTSolution.kt
+++ src/main/kotlin/algorithms/easy/RangeSumOfBSTSolution.kt
@@ -4,12 +4,22 @@ import algorithms.common.TreeNode
 
 // https://leetcode.com/problems/range-sum-of-bst
 class RangeSumOfBSTSolution {
-    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+    /*fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
         if (root == null) return 0
         return if (root.`val` in low..high) {
             root.`val` + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
         } else {
             0 + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
         }
+    }*/
+
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        return root?.let {
+            if (root.`val` in low..high) {
+                root.`val` + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
+            } else {
+                0 + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
+            }
+        } ?: 0
     }
 }
