4d212c9b804afdfcc1a66bcaa45ae25801487561
Armando Ruiz <aranruiz13@gmail.com>
2022-09-26 17:26:48 -0500

    Update Two Sum solution

--- src/main/kotlin/algorithms/easy/TwoSumSolution.kt
+++ src/main/kotlin/algorithms/easy/TwoSumSolution.kt
@@ -2,15 +2,13 @@ package algorithms.easy
 
 // https://leetcode.com/problems/two-sum/
 class TwoSumSolution {
-
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val helperMap = hashMapOf<Int, Int>()
-        nums.forEachIndexed { index, item ->
-            val complement = target - item
-            if (helperMap[complement] == null) {
-                helperMap[item] = index
-            } else {
-                return intArrayOf(helperMap.getOrDefault(complement, 0), index)
+        val verifyNumberMap = hashMapOf<Int, Int>()
+        nums.forEachIndexed { index, number ->
+            verifyNumberMap[target - number]?.let {
+                return intArrayOf(it, index)
+            } ?: run {
+                verifyNumberMap[number] = index
             }
         }
         return intArrayOf()
