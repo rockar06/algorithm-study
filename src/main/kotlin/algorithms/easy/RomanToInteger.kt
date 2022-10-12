db25d02cc0a08dfd940067da0a9c38e5a4c3672d
Armando Ruiz <aranruiz13@gmail.com>
2022-10-11 21:09:27 -0500

    Update roman to integer solution

--- src/main/kotlin/algorithms/easy/RomanToInteger.kt
+++ src/main/kotlin/algorithms/easy/RomanToInteger.kt
@@ -3,29 +3,25 @@ package algorithms.easy
 // https://leetcode.com/problems/roman-to-integer
 class RomanToInteger {
 
-    private val numbersMap = hashMapOf<Char, Int>().apply {
-        put('I', 1)
-        put('V', 5)
-        put('X', 10)
-        put('L', 50)
-        put('C', 100)
-        put('D', 500)
-        put('M', 1000)
-    }
+    private val romanNumerals = hashMapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
 
     fun romanToInt(s: String): Int {
-
+        var counter = s.length - 1
         var result = 0
-        var index = s.length - 1
-        while (index >= 0) {
-            if ((index - 1) >= 0 &&
-                numbersMap.getOrDefault(s[index - 1], 0) < numbersMap.getOrDefault(s[index], 0)
-            ) {
-                result += numbersMap.getOrDefault(s[index], 0) - numbersMap.getOrDefault(s[index - 1], 0)
-                index -= 2
+        while (counter >= 0) {
+            if (romanNumerals.getOrDefault(s[counter], 0) > romanNumerals.getOrDefault(s.getOrNull(counter - 1), 0)) {
+                result += romanNumerals.getOrDefault(s[counter], 0) - romanNumerals.getOrDefault(s.getOrNull(counter - 1), 0)
+                counter -= 2
             } else {
-                result += numbersMap.getOrDefault(s[index], 0)
-                index--
+                result += romanNumerals.getOrDefault(s[counter--], 0)
             }
         }
         return result
