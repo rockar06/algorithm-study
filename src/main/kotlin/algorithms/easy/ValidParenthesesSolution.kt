76765fb4e3b0d5e21d384442a1f6d51007f22976
Armando Ruiz <aranruiz13@gmail.com>
2022-10-25 20:20:10 -0500

    Add another solution for ValidParenthesesSolution

--- src/main/kotlin/algorithms/easy/ValidParenthesesSolution.kt
+++ src/main/kotlin/algorithms/easy/ValidParenthesesSolution.kt
@@ -5,6 +5,23 @@ import java.util.*
 //https://leetcode.com/problems/valid-parentheses/submissions/
 class ValidParenthesesSolution {
     fun isValid(s: String): Boolean {
+        val stackContainer = Stack<Char>()
+        s.forEach { character ->
+            if (character == '(' || character == '{' || character == '[') {
+                stackContainer.push(character)
+            } else if (stackContainer.isNotEmpty() &&
+                (character == ')' && stackContainer.peek() == '(' ||
+                        character == '}' && stackContainer.peek() == '{' ||
+                        character == ']' && stackContainer.peek() == '[')
+            ) {
+                stackContainer.pop()
+            } else {
+                stackContainer.push(character)
+            }
+        }
+        return stackContainer.isEmpty()
+    }
+    /*fun isValid(s: String): Boolean {
         val stackHelper = Stack<Char>()
         s.forEach {
             if (it == '(' || it == '{' || it == '[') {
@@ -22,5 +39,5 @@ class ValidParenthesesSolution {
             }
         }
         return stackHelper.isEmpty()
-    }
+    }*/
 }
