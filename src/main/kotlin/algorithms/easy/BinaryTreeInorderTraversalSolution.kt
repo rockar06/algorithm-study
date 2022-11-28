b259ec0e788929365ef8a8f7f07918d315647096
Armando Ruiz <aranruiz13@gmail.com>
2022-11-28 11:14:20 -0600

    Add iterative solution for inorder traversal

--- src/main/kotlin/algorithms/easy/BinaryTreeInorderTraversalSolution.kt
+++ src/main/kotlin/algorithms/easy/BinaryTreeInorderTraversalSolution.kt
@@ -1,12 +1,13 @@
 package algorithms.easy
 
 import algorithms.common.TreeNode
+import java.util.*
 
 
 // https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
 class BinaryTreeInorderTraversalSolution {
 
-    private val result = arrayListOf<Int>()
+    /*private val result = arrayListOf<Int>()
 
     fun inorderTraversal(root: TreeNode?): List<Int> {
         root?.let {
@@ -14,6 +15,31 @@ class BinaryTreeInorderTraversalSolution {
             result.add(it.`val`)
             inorderTraversal(it.right)
         }
+        return result
+    }*/
+
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+
+        if (root == null) return emptyList()
+
+        val stackOfItems = Stack<TreeNode>()
+        val result = arrayListOf<Int>()
+        var currentNode = root
+
+        while (currentNode != null || stackOfItems.size > 0) {
+
+            while (currentNode != null) {
+                stackOfItems.push(currentNode)
+                currentNode = currentNode.left
+            }
+
+            currentNode = stackOfItems.pop()
+
+            result.add(currentNode.`val`)
+
+            currentNode = currentNode.right
+        }
+
         return result
     }
 }
