d6b97550809a13b849fdabe0dee8a13b49bbdf53
Armando Ruiz <aranruiz13@gmail.com>
2022-11-28 19:36:40 -0600

    Add another solution for same tree

--- src/main/kotlin/algorithms/easy/SameTreeSolution.kt
+++ src/main/kotlin/algorithms/easy/SameTreeSolution.kt
@@ -5,20 +5,12 @@ import algorithms.common.TreeNode
 // https://leetcode.com/problems/same-tree/
 class SameTreeSolution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-        return if (p?.`val` == q?.`val`) {
-            if (p?.left != null || q?.left != null) {
-                if (!isSameTree(p?.left, q?.left)) {
-                    return false
-                }
-            }
-            if (p?.right != null || q?.right != null) {
-                if (!isSameTree(p?.right, q?.right)) {
-                    return false
-                }
-            }
+        return if (p == null && q == null) {
             true
-        } else {
+        } else if (p?.`val` != q?.`val`) {
             false
+        } else {
+            isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
         }
     }
 }
