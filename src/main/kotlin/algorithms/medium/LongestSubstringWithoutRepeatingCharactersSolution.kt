fcd32197a337f726a5536ffd55ca25fdb0ed3980
Armando Ruiz <aranruiz13@gmail.com>
2022-11-29 11:54:38 -0600

    Improve solution for longest substring without repeating characters

--- src/main/kotlin/algorithms/medium/LongestSubstringWithoutRepeatingCharactersSolution.kt
+++ src/main/kotlin/algorithms/medium/LongestSubstringWithoutRepeatingCharactersSolution.kt
@@ -2,25 +2,28 @@ package algorithms.medium
 
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class LongestSubstringWithoutRepeatingCharactersSolution {
-    fun lengthOfLongestSubstring(s: String): Int {
-        var charMap = hashMapOf<Char, Int>()
-        var counter = 0
-        var stringIndex = 0
-        var result = 0
-        var secondaryIndex = 0
 
-        while (stringIndex < s.length && secondaryIndex < s.length) {
-            charMap[s[secondaryIndex]] = (charMap[s[secondaryIndex]] ?: 0) + 1
-            if (charMap[s[secondaryIndex]] == 1) {
-                counter++
-                secondaryIndex++
-            } else {
-                counter = 0
-                charMap.clear()
-                stringIndex++
-                secondaryIndex = stringIndex
+    fun lengthOfLongestSubstring(s: String): Int {
+        val charMapping = hashMapOf<Char, Int>()
+        var leftPointer = 0
+        var rightPointer = 0
+        var result = 0
+
+        while (rightPointer < s.length) {
+            // Get the char from right pointer
+            val rightChar = s[rightPointer]
+            // Add value to map and increase the number of coincidences
+            charMapping[rightChar] = charMapping.getOrDefault(rightChar, 0) + 1
+
+            // If charMapping[rightChar] is greater than 1 iterate until we get only 1
+            while (charMapping.getOrDefault(rightChar, 0) > 1) {
+                val charLeft = s[leftPointer]
+                charMapping[charLeft] = charMapping.getOrDefault(charLeft, 1) - 1
+                leftPointer++
             }
-            result = if (counter > result) counter else result
+            result = Math.max(result, rightPointer - leftPointer + 1)
+
+            rightPointer++
         }
 
         return result
