3ba92679fb40a4a1fdf6e17aeb22e8b1e8a71f04
Armando Ruiz <aranruiz13@gmail.com>
2022-11-28 22:20:22 -0600

    Add another solution for convert sorted array to binary search tree

--- src/main/kotlin/algorithms/easy/ConvertSortedArrayToBinarySearchTreeSolution.kt
+++ src/main/kotlin/algorithms/easy/ConvertSortedArrayToBinarySearchTreeSolution.kt
@@ -4,10 +4,26 @@ import algorithms.common.TreeNode
 
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 class ConvertSortedArrayToBinarySearchTreeSolution {
-    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    /*fun sortedArrayToBST(nums: IntArray): TreeNode? {
         return TreeNode(nums[nums.size / 2]).apply {
             left = if (nums.size > 1) sortedArrayToBST(nums.copyOfRange(0, nums.size / 2)) else null
             right = if (nums.size > 2) sortedArrayToBST(nums.copyOfRange((nums.size / 2) + 1, nums.size)) else null
+        }
+    }*/
+
+    private lateinit var arrayToConvert: IntArray
+
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        arrayToConvert = nums
+        return helperConverter(0, nums.size - 1)
+    }
+
+    private fun helperConverter(leftPointer: Int, rightPointer: Int): TreeNode? {
+        if (leftPointer > rightPointer) return null
+        val middlePointer = (rightPointer + leftPointer) / 2
+        return TreeNode(arrayToConvert[middlePointer]).apply {
+            left = helperConverter(leftPointer, middlePointer - 1)
+            right = helperConverter(middlePointer + 1, rightPointer)
         }
     }
 }
