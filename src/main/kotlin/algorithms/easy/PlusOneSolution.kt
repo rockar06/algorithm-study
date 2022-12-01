1b5d3c80c02b6743cc0366bbd7745d3b1e8c9dee
Armando Ruiz <aranruiz13@gmail.com>
2022-12-01 10:38:36 -0600

    Add another solution for Plus One

--- src/main/kotlin/algorithms/easy/PlusOneSolution.kt
+++ src/main/kotlin/algorithms/easy/PlusOneSolution.kt
@@ -2,7 +2,7 @@ package algorithms.easy
 
 // https://leetcode.com/problems/plus-one/submissions/
 class PlusOneSolution {
-    fun plusOne(digits: IntArray): IntArray {
+    /*fun plusOne(digits: IntArray): IntArray {
         var carriedDigit = false
         var index = digits.size - 1
         var additionResult: Int
@@ -25,6 +25,30 @@ class PlusOneSolution {
                 resultDigits[digitsIndex + 1] = value
             }
             resultDigits
+        } else {
+            digits
+        }
+    }*/
+
+    fun plusOne(digits: IntArray): IntArray {
+        var rightPointer = digits.lastIndex
+        var carryItem = 0
+
+        val currentSum = digits[rightPointer] + 1
+        carryItem = currentSum / 10
+        digits[rightPointer--] = currentSum % 10
+
+        while (carryItem > 0 && rightPointer >= 0) {
+
+            val carrySum = digits[rightPointer] + carryItem
+            carryItem = carrySum / 10
+            digits[rightPointer] = carrySum % 10
+
+            rightPointer--
+        }
+
+        return if (carryItem > 0) {
+            intArrayOf(carryItem) + digits
         } else {
             digits
         }
