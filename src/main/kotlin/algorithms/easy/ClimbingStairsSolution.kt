e5ae4c7394a60b5eda70d36a87c1f53f58ece71e
Armando Ruiz <aranruiz13@gmail.com>
2022-12-12 19:21:34 -0600

    Add another solution for climbing stairs

--- src/main/kotlin/algorithms/easy/ClimbingStairsSolution.kt
+++ src/main/kotlin/algorithms/easy/ClimbingStairsSolution.kt
@@ -2,7 +2,7 @@ package algorithms.easy
 
 // https://leetcode.com/problems/climbing-stairs/
 class ClimbingStairsSolution {
-    fun climbStairs(n: Int): Int {
+    /*fun climbStairs(n: Int): Int {
         val arrayOfNumbers = IntArray(n + 1)
         arrayOfNumbers[0] = 1
         for (i in 1..n) {
@@ -11,6 +11,17 @@ class ClimbingStairsSolution {
             } else {
                 arrayOfNumbers[i] = arrayOfNumbers[i - 1] + arrayOfNumbers[i - 2]
             }
+        }
+        return arrayOfNumbers.last()
+    }*/
+
+    fun climbStairs(n: Int): Int {
+        if (n == 0 || n == 1) return 1
+        val arrayOfNumbers = IntArray(n + 1)
+        arrayOfNumbers[0] = 1
+        arrayOfNumbers[1] = 1
+        for (i in 2..n) {
+            arrayOfNumbers[i] = arrayOfNumbers[i - 1] + arrayOfNumbers[i - 2]
         }
         return arrayOfNumbers.last()
     }
