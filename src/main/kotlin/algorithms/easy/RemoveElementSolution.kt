e4c9488890ea4488664189d2a718441d9abbcdbc
Armando Ruiz <aranruiz13@gmail.com>
2023-01-02 13:25:59 -0600

    Add another solution for Remove element

--- src/main/kotlin/algorithms/easy/RemoveElementSolution.kt
+++ src/main/kotlin/algorithms/easy/RemoveElementSolution.kt
@@ -3,24 +3,21 @@ package algorithms.easy
 // https://leetcode.com/problems/remove-element/
 class RemoveElementSolution {
     fun removeElement(nums: IntArray, `val`: Int): Int {
-        var index = 0
-        var indexNextDifferentNumber = 0
-        var firstNumberBackup = 0
-        var secondNumberBackup = 0
-        while (indexNextDifferentNumber < nums.size) {
-            indexNextDifferentNumber++
-            if (nums[index] != `val`) {
-                index++
-            } else {
-                if (indexNextDifferentNumber < nums.size && nums[indexNextDifferentNumber] != `val`) {
-                    firstNumberBackup = nums[index]
-                    secondNumberBackup = nums[indexNextDifferentNumber]
-                    nums[index] = secondNumberBackup
-                    nums[indexNextDifferentNumber] = firstNumberBackup
-                    index++
+        var leftPointer = 0
+        var rightPointer = nums.lastIndex
+        while (leftPointer <= rightPointer) {
+            if (nums[leftPointer] == `val`) {
+                if (nums[rightPointer] != `val`) {
+                    val tempValue = nums[leftPointer]
+                    nums[leftPointer++] = nums[rightPointer]
+                    nums[rightPointer--] = tempValue
+                } else {
+                    rightPointer--
                 }
+            } else {
+                leftPointer++
             }
         }
-        return index
+        return leftPointer
     }
 }
