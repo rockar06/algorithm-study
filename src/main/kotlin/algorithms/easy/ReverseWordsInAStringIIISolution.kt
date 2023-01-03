8106d2b1f865547b5e1b643c124bc77984133dfb
Armando Ruiz <aranruiz13@gmail.com>
2023-01-03 17:51:43 -0600

    Update solution for Reverse Words in a String III

--- src/main/kotlin/algorithms/easy/ReverseWordsInAStringIIISolution.kt
+++ src/main/kotlin/algorithms/easy/ReverseWordsInAStringIIISolution.kt
@@ -1,59 +1,18 @@
 package algorithms.easy
 
+// https://leetcode.com/problems/reverse-words-in-a-string-iii
 class ReverseWordsInAStringIIISolution {
 
     fun reverseWords(s: String): String {
-        val reverseString = reverseString(s)
-        val listOfStrings = convertStringToList(reverseString)
-        reverseList(listOfStrings)
-        return generateString(listOfStrings)
-    }
-
-    private fun reverseString(stringToReverse: String): String {
-        val stringBuilder = StringBuilder(stringToReverse)
-        var leftIndex = 0
-        var rightIndex = stringBuilder.length - 1
-        while (leftIndex < rightIndex) {
-            val temp = stringBuilder[rightIndex]
-            stringBuilder[rightIndex--] = stringBuilder[leftIndex]
-            stringBuilder[leftIndex++] = temp
-        }
-        return stringBuilder.toString()
-    }
-
-    private fun convertStringToList(stringToConvert: String): MutableList<String> {
-        val stringBuilder = StringBuilder()
-        val listOfStrings = mutableListOf<String>()
-        stringToConvert.forEach {
-            if (it != ' ') {
-                stringBuilder.append(it)
-            } else {
-                listOfStrings.add(stringBuilder.toString())
-                stringBuilder.clear()
+        val resultBuilder = StringBuilder(s.length)
+        val listOfWords = s.split(" ")
+        listOfWords.forEachIndexed { index, word ->
+            val reversedText = word.reversed()
+            resultBuilder.append(reversedText)
+            if (index < listOfWords.lastIndex) {
+                resultBuilder.append(' ')
             }
         }
-        listOfStrings.add(stringBuilder.toString())
-        return listOfStrings
-    }
-
-    private fun reverseList(listToReverse: MutableList<String>) {
-        var leftIndex = 0
-        var rightIndex = listToReverse.size - 1
-        while (leftIndex < rightIndex) {
-            val temp = listToReverse[rightIndex]
-            listToReverse[rightIndex--] = listToReverse[leftIndex]
-            listToReverse[leftIndex++] = temp
-        }
-    }
-
-    private fun generateString(listOfStrings: List<String>): String {
-        val resultString = StringBuilder()
-        listOfStrings.forEachIndexed { index, item ->
-            if (index != 0) {
-                resultString.append(" ")
-            }
-            resultString.append(item)
-        }
-        return resultString.toString()
+        return resultBuilder.toString()
     }
 }
