649197187a699049bac63cb8fe08f0634e79f598
Armando Ruiz <aranruiz13@gmail.com>
2023-01-03 18:42:34 -0600

    Update solution for Remove Duplicates from Sorted Array

--- src/main/kotlin/algorithms/easy/RemoveDuplicatesFromSortedArraySolution.kt
+++ src/main/kotlin/algorithms/easy/RemoveDuplicatesFromSortedArraySolution.kt
@@ -2,21 +2,20 @@ package algorithms.easy
 
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 class RemoveDuplicatesFromSortedArraySolution {
+
     fun removeDuplicates(nums: IntArray): Int {
-        var index = 0
-        var indexNextGreaterNumber = 0
-        var nextNumberBackup = 0
-        var previousNumberBackup = 0
-        while (indexNextGreaterNumber < nums.size) {
-            if (nums[index] >= nums[indexNextGreaterNumber]) {
-                indexNextGreaterNumber++
+        if (nums.size == 1) return 1
+        var leftPointer = 0
+        var rightPointer = 1
+
+        while (rightPointer < nums.size) {
+            if (nums[leftPointer] != nums[rightPointer]) {
+                nums[++leftPointer] = nums[rightPointer++]
             } else {
-                nextNumberBackup = nums[indexNextGreaterNumber]
-                previousNumberBackup = nums[++index]
-                nums[index] = nextNumberBackup
-                nums[indexNextGreaterNumber] = previousNumberBackup
+                rightPointer++
             }
         }
-        return ++index
+
+        return leftPointer + 1
     }
 }
