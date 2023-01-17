adc270372f2dc7d54fb7254ab6982fa41485cd9c
Armando Ruiz <aranruiz13@gmail.com>
2023-01-17 08:58:57 -0600

    Add another solution for Valid Sudoku

--- src/main/kotlin/algorithms/medium/ValidSudokuSolution.kt
+++ src/main/kotlin/algorithms/medium/ValidSudokuSolution.kt
@@ -3,7 +3,7 @@ package algorithms.medium
 // https://leetcode.com/problems/valid-sudoku/
 class ValidSudokuSolution {
 
-    fun isValidSudoku(board: Array<CharArray>): Boolean {
+    /*fun isValidSudoku(board: Array<CharArray>): Boolean {
         board.forEachIndexed { rowIndex, row ->
             row.forEachIndexed { index, char ->
                 if (char != '.') {
@@ -47,5 +47,46 @@ class ValidSudokuSolution {
             if (char == board[currentIndex][itemIndex]) return true
         }
         return false
+    }*/
+
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+
+        val rows = Array(9) {
+            HashSet<Char>()
+        }
+        val columns = Array(9) {
+            HashSet<Char>()
+        }
+        val boxes = Array(9) {
+            HashSet<Char>()
+        }
+
+        for (rowIndex in 0 until 9) {
+            for (columnIndex in 0 until 9) {
+                val currentValue = board[rowIndex][columnIndex]
+
+                if (currentValue == '.') {
+                    continue
+                }
+
+                if (rows[rowIndex].contains(currentValue)) {
+                    return false
+                }
+                rows[rowIndex].add(currentValue)
+
+                if (columns[columnIndex].contains(currentValue)) {
+                    return false
+                }
+                columns[columnIndex].add(currentValue)
+
+                val boxIndex = (rowIndex / 3) * 3 + columnIndex / 3
+                if (boxes[boxIndex].contains(currentValue)) {
+                    return false
+                }
+                boxes[boxIndex].add(currentValue)
+            }
+        }
+
+        return true
     }
 }
