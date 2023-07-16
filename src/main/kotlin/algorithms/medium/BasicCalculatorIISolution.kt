19006c718e46b6a34fd8d3a8d86ba3efb2d835de
Armando Ruiz <aranruiz13@gmail.com>
2023-07-15 20:00:02 -0600

    Update solution for Basic Calculator II

--- src/main/kotlin/algorithms/medium/BasicCalculatorIISolution.kt
+++ src/main/kotlin/algorithms/medium/BasicCalculatorIISolution.kt
@@ -6,56 +6,40 @@ import java.util.*
 class BasicCalculatorIISolution {
 
     fun calculate(s: String): Int {
-        val helperStack = Stack<String>()
-        val listOfString = arrayListOf<String>()
-        val numberBuilder = StringBuilder()
 
-        s.forEach { character ->
-            if (character == '+' || character == '-' || character == '*' || character == '/') {
-                listOfString.add(numberBuilder.toString())
-                numberBuilder.clear()
-                listOfString.add(character.toString())
-            } else {
-                if (character != ' ') {
-                    numberBuilder.append(character)
-                }
-            }
-        }
-
-        listOfString.add(numberBuilder.toString())
-
-        listOfString.forEach {
-            if (helperStack.isNotEmpty() && (helperStack.peek() == "*" || helperStack.peek() == "/")) {
-                val temp = if (helperStack.pop() == "*") {
-                    helperStack.pop().toInt() * it.toInt()
-                } else {
-                    helperStack.pop().toInt() / it.toInt()
-                }
-                helperStack.push(temp.toString())
-            } else {
-                helperStack.push(it)
-            }
-        }
-
-        if (helperStack.size == 1) {
-            return helperStack.pop().toInt()
-        }
-
-        helperStack.reverse()
         var result = 0
-        while (helperStack.isNotEmpty()) {
-            when (val lastChar = helperStack.pop()) {
-                "+" -> {
-                    result += helperStack.pop().toInt()
+        var currentNumber = 0
+        var currentOperation = '+'
+        val resultStack = Stack<Int>()
+
+        for (index in s.indices) {
+            if (s[index].isDigit()) {
+                currentNumber = (currentNumber * 10) + (s[index] - '0')
+            }
+            if (!s[index].isDigit() && !s[index].isWhitespace() || index == s.lastIndex) {
+                when (currentOperation) {
+                    '-' -> {
+                        resultStack.push(-currentNumber)
+                    }
+                    '+' -> {
+                        resultStack.push(currentNumber)
+                    }
+                    '*' -> {
+                        resultStack.push(resultStack.pop() * currentNumber)
+                    }
+                    else -> {
+                        resultStack.push(resultStack.pop() / currentNumber)
+                    }
                 }
-                "-" -> {
-                    result -= helperStack.pop().toInt()
-                }
-                else -> {
-                    result = lastChar.toInt()
-                }
+                currentOperation = s[index]
+                currentNumber = 0
             }
         }
+
+        while (resultStack.isNotEmpty()) {
+            result += resultStack.pop()
+        }
+
         return result
     }
 }
