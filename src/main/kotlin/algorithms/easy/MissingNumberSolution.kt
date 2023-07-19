2c3790b623791df8ee9fa4b6188b2f7b742096d3
Armando Ruiz <aranruiz13@gmail.com>
2023-07-19 14:39:26 -0600

    Add another solution for Missing Number problem

--- src/main/kotlin/algorithms/easy/MissingNumberSolution.kt
+++ src/main/kotlin/algorithms/easy/MissingNumberSolution.kt
@@ -2,31 +2,17 @@ package algorithms.easy
 
 // https://leetcode.com/problems/missing-number/
 class MissingNumberSolution {
-    /*fun missingNumber(nums: IntArray): Int {
-        val numberMap = hashMapOf<Int, Int>()
-        for (i in 0..nums.size) {
-            numberMap[i] = 0
-        }
-        nums.forEach {
-            numberMap[it] = 1
-        }
-        numberMap.forEach { (key, value) ->
-            if (value == 0) {
-                return key
-            }
-        }
-        return 0
-    }*/
+
     fun missingNumber(nums: IntArray): Int {
-        val numberMapper = IntArray(nums.size + 1) { index ->
-            index
+        var result = 0
+        for (index in nums.indices) {
+            result += (index + 1)
         }
-        nums.forEach { item ->
-            numberMapper[item] = -1
+
+        nums.forEach {
+            result -= it
         }
-        numberMapper.forEach { item ->
-            if (item != -1) return item
-        }
-        return 0
+
+        return result
     }
 }
