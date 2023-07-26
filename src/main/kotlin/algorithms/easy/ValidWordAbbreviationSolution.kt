a0be0aa421d7d8b757a04274077de4844896bf76
Armando Ruiz <aranruiz13@gmail.com>
2023-07-26 17:50:41 -0600

    Add another solution for valid word abbreviation problem

--- src/main/kotlin/algorithms/easy/ValidWordAbbreviationSolution.kt
+++ src/main/kotlin/algorithms/easy/ValidWordAbbreviationSolution.kt
@@ -3,66 +3,33 @@ package algorithms.easy
 // https://leetcode.com/problems/valid-word-abbreviation/
 class ValidWordAbbreviationSolution {
 
-    // "internationalization", abbr = "i12iz4n"
-    /*fun validWordAbbreviation(word: String, abbr: String): Boolean {
-        if (abbr.length > word.length) return false
-        var counter = 0
-        var abbrCounter = 0
-
-        while (counter < word.length && abbrCounter < abbr.length) {
-            if (abbr[abbrCounter] in '1'..'9') {
-                val result = getNumberOfCharacters(abbr, abbrCounter)
-                counter += result.first
-                abbrCounter += result.second
-                if (word.getOrNull(counter) != abbr.getOrNull(abbrCounter)) {
-                    return false
-                }
-            } else {
-                if (word.getOrNull(counter) != abbr.getOrNull(abbrCounter)) {
-                    return false
-                }
-                abbrCounter++
-                counter++
-            }
-        }
-        return counter == word.length && abbrCounter == abbr.length
-    }
-
-    private fun getNumberOfCharacters(abbr: String, counter: Int): Pair<Int, Int> {
-        var abbrCounter = 0
-        val numberBuilder = StringBuilder()
-        while (counter + abbrCounter < abbr.length && abbr[counter + abbrCounter] in '0'..'9') {
-            numberBuilder.append(abbr[counter + abbrCounter])
-            abbrCounter++
-        }
-        return Pair(numberBuilder.toString().toInt(), abbrCounter)
-    }*/
-
     fun validWordAbbreviation(word: String, abbr: String): Boolean {
-        var wordIndex = 0
-        var abbreviationIndex = 0
-        while (wordIndex < word.length) {
-            if (abbr.getOrNull(abbreviationIndex) in '1'..'9') {
-                val valuesToAdd = getAbbreviationNumber(abbr, abbreviationIndex)
-                abbreviationIndex += valuesToAdd.first
-                wordIndex += valuesToAdd.second
-            }
-            if (word.getOrNull(wordIndex) != abbr.getOrNull(abbreviationIndex)) return false
-            if (wordIndex < word.length) {
-                wordIndex++
-                abbreviationIndex++
-            }
-        }
-        return wordIndex == word.length && abbreviationIndex == abbr.length
-    }
+        if (abbr.length > word.length) return false
 
-    private fun getAbbreviationNumber(abbreviation: String, startIndex: Int): Pair<Int, Int> {
-        var abbrCounter = startIndex
-        val numberBuilder = StringBuilder()
-        while (abbrCounter < abbreviation.length && abbreviation[abbrCounter] in '0'..'9') {
-            numberBuilder.append(abbreviation[abbrCounter])
-            abbrCounter++
+        var extraIndices = 0
+        var index = 0
+        val digitBuilder = StringBuilder()
+
+        while (index < abbr.length) {
+            if (abbr[index].isDigit()) {
+                if (digitBuilder.isEmpty() && abbr[index] == '0') return false
+                digitBuilder.append(abbr[index])
+            } else {
+                if (digitBuilder.isNotEmpty()) {
+                    extraIndices += digitBuilder.toString().toInt() - digitBuilder.length
+                    digitBuilder.clear()
+                }
+                if (index + extraIndices >= word.length || abbr[index] != word[index + extraIndices]) {
+                    return false
+                }
+            }
+            index++
         }
-        return Pair(numberBuilder.toString().length, numberBuilder.toString().toInt())
+
+        if (digitBuilder.isNotEmpty()) {
+            extraIndices += digitBuilder.toString().toInt() - digitBuilder.length
+        }
+
+        return word.length == index + extraIndices
     }
 }
