6c97e1ca141cf66cea9d7a2e9d4c83443597c7e6
Armando Ruiz <aranruiz13@gmail.com>
2023-07-27 12:26:55 -0600

    Add another solution for range sum of BST problem

--- src/main/kotlin/algorithms/easy/RangeSumOfBSTSolution.kt
+++ src/main/kotlin/algorithms/easy/RangeSumOfBSTSolution.kt
@@ -4,22 +4,9 @@ import algorithms.common.TreeNode
 
 // https://leetcode.com/problems/range-sum-of-bst
 class RangeSumOfBSTSolution {
-    /*fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
-        if (root == null) return 0
-        return if (root.`val` in low..high) {
-            root.`val` + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
-        } else {
-            0 + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
-        }
-    }*/
 
     fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
-        return root?.let {
-            if (root.`val` in low..high) {
-                root.`val` + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
-            } else {
-                0 + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
-            }
-        } ?: 0
+        if (root == null) return 0
+        return (if (root.`val` in (low..high)) root.`val` else 0) + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
     }
 }
