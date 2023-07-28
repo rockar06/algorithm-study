495f0e20d24d47585f56885c7cf28e9b495b98ff
Armando Ruiz <aranruiz13@gmail.com>
2023-07-28 13:13:51 -0600

    Add another solution for Custom Sort String problem

--- src/main/kotlin/algorithms/medium/CustomSortStringSolution.kt
+++ src/main/kotlin/algorithms/medium/CustomSortStringSolution.kt
@@ -4,25 +4,27 @@ package algorithms.medium
 class CustomSortStringSolution {
 
     fun customSortString(order: String, s: String): String {
-        val characterMapper = hashMapOf<Char, Int>()
-        val resultStringBuilder = StringBuilder(s.length)
+        val orderMapper = hashMapOf<Char, Int>()
+        val result = StringBuilder(s.length)
+
         s.forEach {
-            characterMapper[it] = characterMapper.getOrDefault(it, 0) + 1
+            orderMapper[it] = orderMapper.getOrDefault(it, 0) + 1
         }
+
         order.forEach {
-            if (characterMapper[it] != null) {
-                for (index in 0 until characterMapper.getOrDefault(it, 0)) {
-                    resultStringBuilder.append(it)
-                }
-                characterMapper.remove(it)
+            val repetitions = orderMapper[it] ?: 0
+            for (index in 1 .. repetitions) {
+                result.append(it)
+            }
+            orderMapper.remove(it)
+        }
+
+        s.forEach {
+            if (orderMapper[it] != null) {
+                result.append(it)
             }
         }
 
-        characterMapper.entries.forEach {
-            for (index in 0 until it.value) {
-                resultStringBuilder.append(it.key)
-            }
-        }
-        return resultStringBuilder.toString()
+        return result.toString()
     }
 }
